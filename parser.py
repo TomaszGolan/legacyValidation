d0c3073ea90a0726e5e4bba3383f9d9d16db55b0
Tomasz Golan <tomasz.golan@gmail.com>
2016-04-05 12:29:20 -0500

    xsec comparisons

--- parser.py
+++ parser.py
@@ -17,22 +17,11 @@ def getArgs():
                        help = "default = SL6")
   parser.add_argument ("--debug", action = "store", dest = "debug", metavar = "[false or true]", default = "false",
                        help = "default = false")
-                       
+
   required = parser.add_argument_group ("required arguments")
   required.add_argument ("--genie_tag", action = "store", dest = "tag", metavar = "[GENIE VERSION]", required = True)
   required.add_argument ("--run_path", action = "store", dest = "run", metavar = "[PATH TO RUNGENIE]", required = True)
   required.add_argument ("--builds", action = "store", dest = "builds", metavar = "[PATH TO BUILDS]", required = True)
   required.add_argument ("--output", action = "store", dest = "output", metavar = ["PATH FOR OUTPUT]"], required = True)
-
-  # giving up this idea for now, script will look for previous results automatically 
-  #~ previous = parser.add_argument_group ("previous results (will skip steps if provided)")
-  #~ previous.add_argument ("--nucleon_splines", action = "store", dest = "xsec_n", metavar = "[PATH]",
-                         #~ help = "path to nu-N splines")
-  #~ previous.add_argument ("--nucleus_splines", action = "store", dest = "xsec_a", metavar = "[PATH]",
-                         #~ help = "path to nu-A splines")
-  #~ previous.add_argument ("--mctest", action = "store", dest = "mctest", metavar = "[PATH]",
-                         #~ help = "path to standard mctest output")
-  #~ previous.add_argument ("--sanity", action = "store", dest = "sanity", metavar = "[PATH]",
-                         #~ help = "path to standard mctest sanity logs")
 
   return parser.parse_args()
